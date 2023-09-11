384c8e2085ce0b989aafdd809f8394dc2cef0b3f
dem507 <orbikadm@gmail.com>
2023-09-11 20:29:06 +1000

    Реализован бот telegram, рефакторинг некоторых функций

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from dataclasses import dataclass
+import sys
+import os
 import time
-from zeep import Client, Settings
 import sqlite3
 import logging
+from dataclasses import dataclass
+
+from zeep import Client, Settings
 from telegram import Bot
 from telegram.error import TelegramError
-import sys
+from dotenv import load_dotenv
 
+
+load_dotenv()
 API_URL_GET_ORDERS = 'http://api.rossko.ru/service/v2.1/GetOrders'
-TIME_TO_SLEEP = 10
-KEY1 = '84eebe2fb3e7b8a79355efc8cbd772de'
-KEY2 = 'fbb861504dbd6faf3374fc0d3c8a3e87'
+ROSSKO_API_KEY1 = os.getenv('ROSSKO_API_KEY1')
+ROSSKO_API_KEY2 = os.getenv('ROSSKO_API_KEY2')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 DATABASE = 'orders.sqlite3'
-TELEGRAM_TOKEN = ''
-TELEGRAM_CHAT_ID = ''
+TIME_TO_SLEEP = 10
+INSERT_QUERY = """
+    INSERT INTO orders(
+    orderid, created_date, delivery_address, part_number, name, brand,
+    price, count, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -22,6 +32,7 @@ logging.basicConfig(
 )
 handler = logging.StreamHandler(stream=sys.stderr)
 logger.addHandler(handler)
+
 
 @dataclass
 class Part:
@@ -36,22 +47,32 @@ class Part:
     count: int
 
 
-def send_message(part, message):
+def send_message(bot, message):
     """Функция отправляет сообщение в телеграм."""
     try:
         logger.debug('Сообщение отправляется в телеграмм...')
-        # bot.send_message(TELEGRAM_CHAT_ID, message)
+        bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.debug(f'Сообщение успешно отправлено: \n {message}')
     except TelegramError as error:
         logger.error(f'Ошибка при отправке сообщения в телеграмм: {error}')
 
+
 def get_orders_list():
-    settings = Settings(strict=False, xml_huge_tree=True)
-    client = Client(API_URL_GET_ORDERS, settings=settings)
-    return client.service.GetOrders(KEY1=KEY1, KEY2=KEY2, type=4, limit=60).OrdersList.Order
+    try:
+        settings = Settings(strict=False, xml_huge_tree=True)
+        client = Client(API_URL_GET_ORDERS, settings=settings)
+        return client.service.GetOrders(
+            KEY1=ROSSKO_API_KEY1,
+            KEY2=ROSSKO_API_KEY2,
+            type=4,
+            limit=60
+        ).OrdersList.Order
+    except Exception as error:
+        logger.error(f'Ошибка при получении ответа от Api Rossko: {error}')
 
 
 def create_table(cur):
+    """Функция создаёт таблицу в базе данных если её нет."""
     cur.execute("""CREATE TABLE IF NOT EXISTS orders(
         orderid INT,
         created_date TEXT,
@@ -64,8 +85,9 @@ def create_table(cur):
         status INT);
     """)
 
+
 def parse_order(order) -> list[Part]:
-    """Парсим заказ, возвращаем объект Part"""
+    """Парсим заказ, возвращаем объект Part."""
     parts_list = []
     orderid = order.id
     created_date = order.created_date
@@ -80,95 +102,80 @@ def parse_order(order) -> list[Part]:
             brand = part.brand
             price = part.price
             count = part.count
-
             newpart = Part(
-                orderid=orderid, created_date=created_date, delivery_address=delivery_address,
-                part_number=part_number, name=name, brand=brand, price=price, count=count, status=status)
+                orderid=orderid, created_date=created_date, status=status,
+                delivery_address=delivery_address, part_number=part_number,
+                name=name, brand=brand, price=price, count=count)
             parts_list.append(newpart)
-
     return parts_list
-
-def check_order(order):
-    """Возвращает True если заказ есть в БД."""
-    return False
 
 
 def get_parts_from_db(cur):
+    """Функция получает из базы даных все товары, по которым была отмена."""
     check_query = """SELECT * FROM orders WHERE status LIKE 7"""
     cur.execute(check_query)
     db_parts = cur.fetchall()
-    # print(db_parts)
     return db_parts
-
-
-def first_save(con, part):
-    values = (part.orderid, part.created_date, part.delivery_address, part.part_number, part.name,
-              part.brand, part.price, part.count, part.status)
-    query_1 = """
-        INSERT INTO orders(
-        orderid, created_date, delivery_address, part_number, name, brand, price, count, status
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"""
-    cur.execute(query_1, values)
-    con.commit()
 
 
 def save_to_bd(con, part):
     """Сохраняет заказ в БД."""
     values = (
-        part.orderid, part.created_date, part.delivery_address, part.part_number, part.name,
-        part.brand, part.price, part.count, part.status)
-    query_1 = """
-        INSERT INTO orders(
-        orderid, created_date, delivery_address, part_number, name, brand, price, count, status
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"""
-    cur.execute(query_1, values)
+        part.orderid, part.created_date, part.delivery_address,
+        part.part_number, part.name, part.brand, part.price, part.count,
+        part.status
+        )
+    cur.execute(INSERT_QUERY, values)
     con.commit()
-
 
 
 def get_message(order):
     """Подготавливаем сообщение для отправки."""
-    return 'Заказ сняли из-за отсутствия на складе'
+    message = f"""
+\u2757\u2757\u2757 ОТМЕНА ПОЗИЦИИ \u2757\u2757\u2757\n
+\U0001F4CBЗаказ:  {order.orderid}\n
+\u23F0Дата заказа:  {part.created_date}\n
+\U0001F4EDАдрес доставки:  {part.delivery_address}\n
+\u274C Отменено:
+    Артикул:  {part.part_number}
+    Бренд:  {part.brand}
+    \U0001F449Наименование:  {part.name} - {part.count} шт.
 
-
+    """
+    return message
 
 
 if __name__ == '__main__':
-    # bot: Bot = Bot(token=TELEGRAM_TOKEN)
+    bot: Bot = Bot(token=TELEGRAM_TOKEN)
     while True:
         try:
-            # print('Получаем список заказов от Api')
             orders_list = get_orders_list()
-            # print('Успешно получили список заказов от Api')
-            #  Проверяем есть ли данные в бд, если есть пропускаем, если нет - записываем в бд и отправляем сообщение
             con = sqlite3.connect(DATABASE)
             cur = con.cursor()
             create_table(cur)
 
             for order in orders_list:
-                # print('Запущен цикл')
-                # print(order)
                 part_list = parse_order(order)
                 print('список запчастюль', part_list)
                 parts_in_db = get_parts_from_db(cur)
                 for part in part_list:
                     if not parts_in_db:
-                        first_save(con=con, part=part)
+                        save_to_bd(con=con, part=part)
+                        message = get_message(part)
+                        send_message(bot, message)
 
                     for item in parts_in_db:
                         if part.orderid in item and part.part_number in item:
                             print('НЕ Записываем')
                         else:
                             save_to_bd(con=con, part=part)
-
-                # message = get_message(order)
-                # print('Сообщение подготовлено')
-                # send_message(message)
-                # print('Сообщение отправлено')
+                            message = get_message(part)
+                            send_message(bot, message)
         except Exception as error:
             print(error)
             error_message = f'Произошла ошибка в работе скрипта\n{error}'
-            send_message(error_message)
-        print('Готовимся спать')
+            send_message(bot, error_message)
+        logger.debug(
+            'Заказы проверены, следующая проверка через {TIME_TO_SLEEP} секунд'
+            )
         time.sleep(TIME_TO_SLEEP)
-        print('Поспали, начинаем новый круг')
